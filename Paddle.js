50ab033a91874f9347303c6413c38b74a8b980b7
Mohamed Razeen <65271268+MohamedRazeen102006@users.noreply.github.com>
2020-05-13 11:50:28 +0530

    Delete Paddle.js

--- Paddle.js
+++
@@ -1,3 +0,0 @@
-class Paddle{constructor(xPos,yPos){this.x = xPos;this.y = yPos;this.height = 100;this.width = 10;}
-displayPaddle(){rect(this.x,this.y,this.width,this.height);var r = map(this.x,0,400,0,255);var g = map(this.y,0,400,0,255);fill(r,g,150);}}
-  
